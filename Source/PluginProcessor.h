40054c9e928b5622e3f84d8d3b958c39ca9d35aa
yarivtenta-creator <yariv.tenta@gmail.com>
2026-08-09 22:19:22 +0200

    Add Source/PluginProcessor.h

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <juce_audio_processors/juce_audio_processors.h>
+#include "DSP/FilterEngine.h"
+#include "DSP/KillBandProcessor.h"
+#include "Parameters.h"
+
+class VintageDualFilterAudioProcessor final : public juce::AudioProcessor
+{
+public:
+    VintageDualFilterAudioProcessor();
+    void prepareToPlay(double, int) override;
+    void releaseResources() override {}
+    bool isBusesLayoutSupported(const BusesLayout&) const override;
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override { return true; }
+    const juce::String getName() const override { return JucePlugin_Name; }
+    bool acceptsMidi() const override { return false; }
+    bool producesMidi() const override { return false; }
+    bool isMidiEffect() const override { return false; }
+    double getTailLengthSeconds() const override { return 0.0; }
+    int getNumPrograms() override { return 5; }
+    int getCurrentProgram() override { return currentProgram; }
+    void setCurrentProgram(int) override;
+    const juce::String getProgramName(int) override;
+    void changeProgramName(int, const juce::String&) override {}
+    void getStateInformation(juce::MemoryBlock&) override;
+    void setStateInformation(const void*, int) override;
+    juce::AudioProcessorValueTreeState parameters;
+
+private:
+    FilterEngine::Settings readSettings(int) const;
+    std::array<FilterEngine, 2> filters;
+    KillBandProcessor killBands;
+    juce::AudioBuffer<float> parallelBuffer;
+    juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> parallelLatencyCompensation{4096};
+    juce::dsp::Gain<float> inputGain, outputGain;
+    int currentProgram = 0;
+    float filterLatency = 0.f;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VintageDualFilterAudioProcessor)
+};
